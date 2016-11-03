9c6dd2617dfb52ee26837210306a083d0057d609
Rudmar <rudmar_junior@hotmail.com>
2016-11-03 12:26:28 -0200

    Funcionalidade On corrigida, adicionada funcionalidade Off ao seu botao específico

--- app/src/main/java/app/gamadrone/MainActivity.java
+++ app/src/main/java/app/gamadrone/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import app.gamadrone.R;
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     public static int SELECT_PAIRED_DEVICE = 2;
     public static int SELECT_DISCOVERED_DEVICE = 3;
     public static TextView statusMessage;
+    BluetoothAdapter bluetooth = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +24,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         statusMessage = (TextView) findViewById(R.id.statusMessage);
+        bluetooth = BluetoothAdapter.getDefaultAdapter();
     }
 
     public void turnOn(View view) {
 
-        BluetoothAdapter bluetooth;
-        bluetooth = BluetoothAdapter.getDefaultAdapter();
-
         if (bluetooth == null){
-            statusMessage.setText("Bluetooth nao funcional");
+            Toast.makeText(getApplicationContext(), "Este dispositivo nao suporta Bluetooth", Toast.LENGTH_SHORT).show();
         } else {
-            statusMessage.setText("Bluetooth funcional");
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+            Toast.makeText(getApplicationContext(), "Bluetooth ativado", Toast.LENGTH_SHORT).show();
         }
 
-        Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        Intent search = new Intent(BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED);
+    }
 
+    public void turnOff(View view) {
+        if(bluetooth.isEnabled()){
+            bluetooth.disable();
+            Toast.makeText(getApplicationContext(), "Bluetooth desligado", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Bluetooth já estava desligado", Toast.LENGTH_LONG).show();
+        }
     }
 
 }
